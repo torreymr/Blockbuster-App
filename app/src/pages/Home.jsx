664fbbf06066e26562b3d0cb16bc9c4140a8f885
torreymr <torreymramirez@gmail.com>
2023-12-29 14:43:58 -0700

    Added tailwind

---
+++ app/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="bg-gradient-to-t from-[#30cfd0] to-[#330867] h-[100vh]">
+        Hello
+      </h1>
+    </>
+  );
+};
+
+export default Home;
