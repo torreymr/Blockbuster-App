7dc05d50e671523c98e6f4ebc5357a582247ae6e
torreymr <torreymramirez@gmail.com>
2024-01-02 12:51:43 -0700

    Fixed height on page

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "../components/Navbar.jsx";
 import Searchbar from "../components/Searchbar.jsx";
 import MainRow from "../components/MainRow.jsx";
+import Movie from "../components/Movie.jsx";
 
 const Home = () => {
   const [isDropdownVisible, setDropdownVisible] = useState(false);
@@ -18,9 +19,10 @@ const Home = () => {
           isActive={isDropdownVisible}
         />
       )}
-      <div className="background-color h-[100vh] px-[1rem]">
+      <div className="background-color min-h-[100vh] px-[1rem]">
         <Navbar toggleDropdown={toggleDropdown} />
         <MainRow />
+        <Movie />
       </div>
     </>
   );
