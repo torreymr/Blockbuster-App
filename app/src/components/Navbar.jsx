c04f883ecf16bbf6e63530a5c54ff0d661998a43
torreymr <torreymramirez@gmail.com>
2024-01-01 17:40:32 -0700

    Bolded Nav Logo text

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({ toggleDropdown, isActive }) => {
       <div className="flex justify-between items-center py-[1.5rem] gap-[4rem] text-gray-200 px-[3%] md:px-[5%]">
         <div className="w-[5rem] flex flex-col items-center text-center  cursor-pointer">
           <BiSolidMoviePlay className="w-[3rem] h-auto" />
-          <p className="text-wrap text-sm">WE LOVE MOVIES</p>
+          <p className="text-wrap text-sm font-bold">WE LOVE MOVIES</p>
         </div>
         <div className="w-[100%] hidden md:flex items-center gap-4">
           <input className="rounded-full w-[100%] h-[2.5rem] px-[1rem] text-xl outline-none bg-gray-800 text-gray-500" />
