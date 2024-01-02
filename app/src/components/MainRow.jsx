4a6c6efd1d28befc81518d936f42ebf95acb4f29
torreymr <torreymramirez@gmail.com>
2024-01-02 08:50:45 -0700

    Main Row Complete for most part

--- app/src/components/MainRow.jsx
+++ app/src/components/MainRow.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import requests from "../Requests";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const MainRow = () => {
   const [movies, setMovies] = useState([]);
@@ -11,22 +12,46 @@ const MainRow = () => {
     });
   }, []);
 
+  const slideLeft = () => {
+    const slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    const slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <>
       <div className="text-white pt-[1rem] flex flex-col items-center">
-        <h1 className="text-4xl md:text-5xl font-bold">What's Popular?</h1>
-        <div className="w-full h-[600px] flex flex-col justify-center">
-          <div
-            id={"slider"}
-            className="h-[65%] flex items-center overflow-x-scroll"
-          >
-            {movies.map((item, id) => (
-              <img
-                src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
-                alt={item?.title}
-                className="h-full object-cover"
-              />
-            ))}
+        <h1 className="text-4xl md:text-5xl font-bold pb-[1rem]">
+          What's Popular?
+        </h1>
+        <div className="w-full h-[600px] flex items-center">
+          <div className="w-full h-[500px] flex items-center relative group">
+            <FaArrowLeft
+              className="absolute hidden left-0 group-hover:block opacity-50 hover:opacity-100 cursor-pointer"
+              size={30}
+              onClick={slideLeft}
+            />
+            <div
+              id={"slider"}
+              className="h-[100%] flex items-center overflow-x-scroll scroll-smooth scrollbar-hide whitespace-nowrap gap-[1.5rem]"
+            >
+              {movies.map((item, id) => (
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${item?.poster_path}`}
+                  alt={item?.title}
+                  className="h-full object-cover cursor-pointer"
+                />
+              ))}
+            </div>
+            <FaArrowRight
+              className="absolute hidden right-0 group-hover:block opacity-50 hover:opacity-100 cursor-pointer"
+              size={30}
+              onClick={slideRight}
+            />
           </div>
         </div>
       </div>
