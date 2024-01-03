e29e8f17e33aab8af94922acbcfd3d6f3b23f8a8
torreymr <torreymramirez@gmail.com>
2024-01-03 15:29:23 -0700

    Position Fixed Navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { IoMdSearch } from "react-icons/io";
 const Navbar = ({ toggleDropdown, isActive }) => {
   return (
     <>
-      <div className="flex justify-between items-center py-[1.5rem] gap-[4rem] text-gray-200 px-[3%] md:px-[5%]">
+      <div className="flex justify-between items-center py-[1.5rem] gap-[4rem] text-gray-200 px-[3%] md:px-[5%] border-b-2 border-gray-800 fixed top-0 left-0 w-full background-color z-50">
         <div className="w-[5rem] flex flex-col items-center text-center  cursor-pointer">
           <BiSolidMoviePlay className="w-[3rem] h-auto" />
           <p className="text-wrap text-sm font-bold">WE LOVE MOVIES</p>
