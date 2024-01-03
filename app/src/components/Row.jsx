bb2307f2aaab2728d6a97755dc4ea030fb814709
torreymr <torreymramirez@gmail.com>
2024-01-03 15:46:07 -0700

    Row optimized to margin

--- app/src/components/Row.jsx
+++ app/src/components/Row.jsx
@@ -25,7 +25,7 @@ const Row = ({ title, fetchURL, rowID }) => {
     <div className="text-white">
       <div className="h-[500px] flex flex-col items-center w-full justify-center">
         <h1 className="text-3xl font-bold pb-[1rem]">{title}</h1>
-        <div className="flex w-full items-center justify-center gap-[1rem]">
+        <div className="flex w-full items-center gap-[1rem]">
           <FaArrowLeft
             className="opacity-50 hover:opacity-100 cursor-pointer hidden md:block"
             size={30}
@@ -33,7 +33,7 @@ const Row = ({ title, fetchURL, rowID }) => {
           />
           <div
             id={"slider" + rowID}
-            className="overflow-x-scroll scrollbar-hide flex gap-[1rem] w-[85%] scroll-smooth"
+            className="overflow-x-scroll scrollbar-hide flex gap-[1rem] w-[85%] scroll-smooth flex-1"
           >
             {movies.map((item, id) => (
               <img
