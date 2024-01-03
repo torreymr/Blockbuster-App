4edd50b90e8baf026d846a17cd9577afc581a7e3
torreymr <torreymramirez@gmail.com>
2024-01-03 15:54:37 -0700

    Movie optimized to margin

--- app/src/components/Movie.jsx
+++ app/src/components/Movie.jsx
@@ -16,21 +16,19 @@ const Movie = () => {
     });
   }, []);
   return (
-    <div className="w-full h-[500px] md:h-[900px] md:px-[5rem] flex items-center">
-      <div className="w-full h-[90%] relative">
-        <img
-          src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
-          alt={movie?.title}
-          className="object-cover h-full w-full"
-        />
-        <div className="h-full w-full bg-gradient-to-r from-black opacity-50 flex flex-col justify-end absolute top-0 left-0"></div>
-        <div className="h-full w-full absolute top-0 left-0 flex flex-col gap-[1rem] md:pb-[4rem] md:pl-[3rem] justify-end text-white pl-[1rem] pb-[2rem]">
-          <h1 className=" text-3xl font-bold md:text-7xl">{movie?.title}</h1>
-          <p className="hidden md:block text-lg w-[70%]">{movie?.overview}</p>
-          <button className="bg-white w-[8rem] h-[3rem] md:w-[11rem] md:h-[4rem] text-black rounded-lg font-bold">
-            Watch Now
-          </button>
-        </div>
+    <div className="w-full h-[500px] md:h-[700px] flex items-center relative">
+      <img
+        src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
+        alt={movie?.title}
+        className="object-cover h-full w-full"
+      />
+      <div className="h-full w-full bg-gradient-to-r from-black opacity-50 flex flex-col justify-end absolute top-0 left-0"></div>
+      <div className="h-full w-full absolute top-0 left-0 flex flex-col gap-[1rem] md:pb-[4rem] md:pl-[3rem] justify-end text-white pl-[1rem] pb-[2rem]">
+        <h1 className=" text-3xl font-bold md:text-7xl">{movie?.title}</h1>
+        <p className="hidden md:block text-lg w-[70%]">{movie?.overview}</p>
+        <button className="bg-white w-[8rem] h-[3rem] md:w-[11rem] md:h-[4rem] text-black rounded-lg font-bold">
+          Watch Now
+        </button>
       </div>
     </div>
   );
