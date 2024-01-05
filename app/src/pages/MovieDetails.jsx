b233e7f179b59f8540c6225700e3b29b7dce5bd6
torreymr <torreymramirez@gmail.com>
2024-01-05 13:03:10 -0700

    Better way to get request

--- app/src/pages/MovieDetails.jsx
+++ app/src/pages/MovieDetails.jsx
@@ -1,19 +1,16 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import requests from "../Requests";
 
 const MovieDetails = () => {
   const [movie, setMovie] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/${id}?api_key=a7bafceb86b756cbdcdc04b163d81543`
-      )
-      .then((response) => {
-        setMovie(response.data);
-      });
+    axios.get(requests.requestMovie(id)).then((response) => {
+      setMovie(response.data);
+    });
   });
 
   return (
