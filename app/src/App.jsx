c9ad1631f776216e6f07bcab17be7e4b25cbde26
torreymr <torreymramirez@gmail.com>
2024-01-05 08:00:38 -0700

    Routes in application

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,16 @@
 import Home from "./pages/Home";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MovieDetails from "./pages/MovieDetails";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/details" Component={MovieDetails} />
+      </Routes>
+    </Router>
   );
 }
 
