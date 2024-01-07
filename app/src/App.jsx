3193af3bd7c33f64345122ab47fc6f87d328510a
torreymr <torreymramirez@gmail.com>
2024-01-06 19:55:37 -0700

    Some basic set up for movieoverview page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,14 @@
 import Home from "./pages/Home";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MovieDetails from "./pages/MovieDetails";
+import MovieOverview from "./pages/MovieOverview";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" Component={Home} />
-        <Route path="/details/:id" Component={MovieDetails} />
+        <Route path="/details/:id" Component={MovieOverview} />
       </Routes>
     </Router>
   );
---
+++ app/src/pages/MovieOverview.jsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import requests from "../Requests.js";
+import Navbar from "../components/Navbar.jsx";
+import Searchbar from "../components/Searchbar.jsx";
+
+const MovieOverview = () => {
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
+
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
+  useEffect(() => {
+    axios.get(requests.requestMovie(id)).then((response) => {
+      setMovie(response.data);
+    });
+  });
+
+  return (
+    <>
+      <div className="w-full background-color min-h-[100vh]">
+        {isDropdownVisible && (
+          <Searchbar
+            toggleDropdown={toggleDropdown}
+            isActive={isDropdownVisible}
+          />
+        )}
+        <Navbar toggleDropdown={toggleDropdown} />
+        <div className="h-full w-full md:px-[8rem] px-[1rem] pb-[2rem] pt-[8rem]">
+          <img
+            src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
+            alt={movie?.title}
+            className="h-[45rem] w-full object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MovieOverview;
