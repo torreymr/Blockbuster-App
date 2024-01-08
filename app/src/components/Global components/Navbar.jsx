12b1de36cda83406cfd488445e22056607eb5c7c
torreymr <torreymramirez@gmail.com>
2024-01-08 13:21:46 -0700

    Nav Logo navigates to home

--- app/src/components/Global components/Navbar.jsx
+++ app/src/components/Global components/Navbar.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { BiSolidMoviePlay } from "react-icons/bi";
 import { IoMdSearch } from "react-icons/io";
 
 const Navbar = ({ toggleDropdown }) => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate(`/`);
+  };
+
   return (
     <>
       <div className="text-gray-200 md:px-[8rem] px-[1rem] fixed top-0 left-0 w-full background-color z-50">
         <div className="flex justify-between items-center py-[1.5rem] gap-[4rem]  border-b-2 border-gray-800">
-          <div className="w-full flex items-center gap-[1rem] cursor-pointer">
+          <div
+            className="w-full flex items-center gap-[1rem] cursor-pointer"
+            onClick={handleLogoClick}
+          >
             <BiSolidMoviePlay className="w-[3rem] md:w-[4rem] h-auto" />
             <p className="text-1xl md:text-3xl w-fit font-thin text-nowrap tracking-wider">
               WE LOVE MOVIES
