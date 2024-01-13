239508ffd2248f1ac7a4f478d579c3aaf6ac00a9
torreymr <torreymramirez@gmail.com>
2024-01-12 17:12:18 -0700

    Main complete excluding providers

--- app/src/components/Movie-Overview-Components/Main.jsx
+++ app/src/components/Movie-Overview-Components/Main.jsx
@@ -6,7 +6,8 @@ import Movie from "../Home-Page-Components/Movie";
 
 const Main = () => {
   const [movie, setMovie] = useState(null);
-  const [streamable, setStreamable] = useState(true);
+  const [streamable, setStreamable] = useState(false);
+  const year = movie?.release_date.split("-")[0];
 
   const { id: movieID } = useParams();
 
@@ -19,13 +20,13 @@ const Main = () => {
   }, []);
 
   return (
-    <div className="h-[45rem] w-full relative">
+    <div className="h-[15rem] lg:h-[40rem] w-full relative">
       <img
         src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
         alt={movie?.title}
         className="h-full w-full object-cover absolute top-0 left-0"
       />
-      <div className="bg-gradient-to-r from-black h-full w-full absolute top-0 left-0"></div>
+      <div className=" h-full w-full absolute top-0 left-0"></div>
       <div className="h-full w-full absolute left-0 top-0 flex flex-col justify-between text-white">
         {streamable ? (
           <div className="bg-black bg-opacity-50 py-2 font-bold flex items-center justify-center gap-4">
@@ -34,9 +35,6 @@ const Main = () => {
         ) : (
           <div></div>
         )}
-        <div>
-          <p>Main stuff</p>
-        </div>
       </div>
     </div>
   );
